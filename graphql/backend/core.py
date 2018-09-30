bb46d5b73017e66a962cfc93c977c63105dcad72
Eran Kampf <eran@ekampf.com>
2018-09-30 13:39:25 -0700

    Tracing middleware

--- graphql/backend/core.py
+++ graphql/backend/core.py
@@ -2,6 +2,7 @@ from functools import partial
 from six import string_types
 
 from ..execution import execute, ExecutionResult
+from ..execution.tracing import TracingMiddleware
 from ..language.base import parse, print_ast
 from ..language import ast
 from ..validation import validate
@@ -19,42 +20,65 @@ if False:  # flake8: noqa
 def execute_and_validate(
     schema,  # type: GraphQLSchema
     document_ast,  # type: Document
+    tracing_middleware, # type: TracingMiddleware
     *args,  # type: Any
     **kwargs  # type: Any
 ):
     # type: (...) -> Union[ExecutionResult, Observable]
-    do_validation = kwargs.get("validate", True)
-    if do_validation:
-        validation_errors = validate(schema, document_ast)
-        if validation_errors:
-            return ExecutionResult(errors=validation_errors, invalid=True)
 
-    return execute(schema, document_ast, *args, **kwargs)
+    tracing_middleware.validation_start()
+    try:
+        do_validation = kwargs.get("validate", True)
+        if do_validation:
+            validation_errors = validate(schema, document_ast)
+            if validation_errors:
+                return ExecutionResult(errors=validation_errors, invalid=True)
+    finally:
+        tracing_middleware.validation_end()
+
+    if tracing_middleware.enabled:
+        middleware = kwargs.get("middleware") or []
+        middleware.append(tracing_middleware)
+        kwargs["middleware"] = middleware
+
+    result = execute(schema, document_ast, *args, **kwargs)
+    if tracing_middleware.enabled:
+        result.extensions["tracing"] = tracing_middleware.tracing_dict
+
+    return result
 
 
 class GraphQLCoreBackend(GraphQLBackend):
     """GraphQLCoreBackend will return a document using the default
     graphql executor"""
 
-    def __init__(self, executor=None):
+    def __init__(self, executor=None, tracing=False):
         # type: (Optional[Any]) -> None
         self.execute_params = {"executor": executor}
+        self.tracing_middleware = TracingMiddleware(tracing)
 
     def document_from_string(self, schema, document_string):
         # type: (GraphQLSchema, Union[Document, str]) -> GraphQLDocument
-        if isinstance(document_string, ast.Document):
-            document_ast = document_string
-            document_string = print_ast(document_ast)
-        else:
-            assert isinstance(
-                document_string, string_types
-            ), "The query must be a string"
-            document_ast = parse(document_string)
-        return GraphQLDocument(
-            schema=schema,
-            document_string=document_string,
-            document_ast=document_ast,
-            execute=partial(
-                execute_and_validate, schema, document_ast, **self.execute_params
-            ),
-        )
+
+        self.tracing_middleware.parsing_start()
+        try:
+            if isinstance(document_string, ast.Document):
+                document_ast = document_string
+                document_string = print_ast(document_ast)
+            else:
+                assert isinstance(
+                    document_string, string_types
+                ), "The query must be a string"
+                document_ast = parse(document_string)
+
+
+            return GraphQLDocument(
+                schema=schema,
+                document_string=document_string,
+                document_ast=document_ast,
+                execute=partial(
+                    execute_and_validate, schema, document_ast, self.tracing_middleware, **self.execute_params
+                ),
+            )
+        finally:
+            self.tracing_middleware.parsing_end()
---
+++ graphql/execution/tracing.py
@@ -0,0 +1,102 @@
+import sys
+import time
+
+PY37 = sys.version_info[0:2] >= (3, 7)
+
+
+class TracingMiddleware(object):
+    DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+    def __init__(self, enabled):
+        self.enabled = enabled
+        self.resolver_stats = list()
+        self.start_time = None
+        self.end_time = None
+        self.parsing_start_time = None
+        self.parsing_end_time = None
+        self.validation_start_time = None
+        self.validation_end_time = None
+
+    def start(self):
+        self.start_time = self.now()
+
+    def end(self):
+        self.end_time = self.now()
+
+    def parsing_start(self):
+        self.parsing_start_time = self.now()
+
+    def parsing_end(self):
+        self.parsing_end_time = self.now()
+
+    def validation_start(self):
+        self.validation_start_time = self.now()
+
+    def validation_end(self):
+        self.validation_end_time = self.now()
+
+    def now(self):
+        if PY37:
+            return time.time_ns()
+
+        return int(time.time() * 1000000000)
+
+    @property
+    def start_time_str(self):
+        return time.strftime(self.DATETIME_FORMAT, time.gmtime(self.start_time/1000))
+
+    @property
+    def end_time_str(self):
+        return time.strftime(self.DATETIME_FORMAT, time.gmtime(self.end_time/1000))
+
+    @property
+    def duration(self):
+        if not self.end_time:
+            raise ValueError("Tracing has not ended yet!")
+
+        return self.end_time - self.start_time
+
+    @property
+    def tracing_dict(self):
+        result = dict(
+            version=1,
+            startTime=self.start_time_str,
+            endTime=self.end_time_str,
+            duration=self.duration,
+            parsing=dict(
+                startOffset=self.parsing_start_time - self.start_time,
+                duration=self.parsing_end_time - self.parsing_start_time,
+            ),
+            execution=dict(
+                resolvers=self.resolver_stats
+            )
+        )
+
+        if self.validation_start_time and self.validation_end_time:
+            result["validation"] = dict(
+                startOffset=self.validation_start_time - self.start_time,
+                duration=self.validation_end_time - self.validation_start_time,
+            )
+
+        return result
+
+    def resolve(self, _next, root, info, *args, **kwargs):
+        if not self.enabled:
+            return _next(root, info, *args, **kwargs)
+
+        start = time.time()
+        try:
+            return _next(root, info, *args, **kwargs)
+        finally:
+            end = time.time()
+            elapsed_ms = (end - start) * 1000
+
+            stat = {
+                "path": info.path,
+                "parentType": str(info.parent_type),
+                "fieldName": info.field_name,
+                "returnType": str(info.return_type),
+                "startOffset": (time.time() * 1000) - self.start_time,
+                "duration": elapsed_ms,
+            }
+            self.resolver_stats.append(stat)
